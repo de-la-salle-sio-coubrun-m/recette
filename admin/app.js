d82eac59f19d72c9266ec586e62f19deb6dd6c11
Damien GAULTIER <dgaultier@FORMATION.lan>
2019-11-21 17:15:14 +0100

    auto_complete presque terminer

--- admin/app.js
+++ admin/app.js
@@ -1,42 +1,24 @@
-var input = document.getElementById('idCategorie');
-var results;
-
-if (window.dataList) {
-    // dataList existe
-    console.log(dataList);
-}
-// functions
-function autocomplete(val) {
-  var dataList_return = [];
-
-  for (i = 0; i < dataList.length; i++) {
-      console.log(dataList[i].toLowerCase().match(val));
-    if (dataList[i].toLowerCase().match(val)) {
-      dataList_return.push(dataList[i]);
-    }
-  }
-
-  return dataList_return;
-}
+var inputs = document.querySelectorAll('input');
 
 // events
-input.onkeyup = function(e) {
-  input_val = this.value; // updates the variable on each ocurrence
-
-  if (input_val.length > 0) {
-    var dataList_to_show = [];
-
-    autocomplete_results = document.getElementById("hugeList");
-    autocomplete_results.innerHTML = '';
-    dataList_to_show = autocomplete(input_val);
-    
-    for (i = 0; i < dataList_to_show.length; i++) {
-      autocomplete_results.innerHTML += '<option>' + dataList_to_show[i] + '</option>';
-
+for (var input of inputs ){
+    input.onkeyup = function(e) {
+        input_name = this.name;
+        input_val = this.value; // updates the variable on each ocurrence
+        let nomChamp = 'nom'+input_name[0].toUpperCase()+input_name.substr(1);
+        if (input_val.length > 0) {
+        fetch('http://localhost/recette/admin/admin.php?action=recette&nomTable='+input_name+'&nomChamps='+nomChamp+'&search='+input_val)
+            .then(function (response) {
+                return response.json();
+            }).then(function (liste) {
+                autocomplete_results = document.getElementById("hugeList_"+input_name);
+                autocomplete_results.innerHTML = '';
+                for (i = 0; i < liste.length; i++) {
+                    autocomplete_results.innerHTML += '<option>' + liste[i] + '</option>';
+                }
+                autocomplete_results.style.display = 'block';
+            });            
+            
+        }
     }
-    autocomplete_results.style.display = 'block';
-  } else {
-    dataList_to_show = [];
-    autocomplete_results.innerHTML = '';
-  }
 }
--- admin/auto_complete.php
+++ admin/auto_complete.php
@@ -1,5 +1,22 @@
 <?php
 
+
+if (isset($_GET['nomTable']) && isset($_GET['nomChamps'])) {
+    $requete = "SELECT ". $_GET['nomChamps'] ." FROM ".$_GET['nomTable'];
+    if (isset($_GET['search'])) {
+        $requete .= " WHERE ".$_GET['nomChamps']." LIKE '%".$_GET['search']."%'";
+    }
+    $resultat=mysqli_query($connexion,$requete);
+    $array = [];
+    while($tab=mysqli_fetch_array($resultat)) {
+        $array[] =  $tab[0];
+    }
+    print json_encode($array);
+    die;
+}
+
+
+
 $array = '[';
 $requete = "SELECT nomCategorie FROM categorie";
 $resultat=mysqli_query($connexion,$requete);
