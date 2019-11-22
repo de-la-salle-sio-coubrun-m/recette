ae95fa5d1e2dc51b5b379c5e1aacc13daf1192a2
Damien GAULTIER <dgaultier@FORMATION.lan>
2019-11-22 13:53:31 +0100

    possibilité de clic avec auto_complete

--- admin/app.js
+++ admin/app.js
@@ -7,21 +7,23 @@ for (var input of inputs ){
         inputTab = this.getAttribute('data-tab');
         inputVal = this.value; // updates the variable on each ocurrence
         //let nomChamp = 'nom'+input_name[0].toUpperCase()+input_name.substr(1);
-        console.log(inputTab, inputName, inputVal);
         if (inputVal.length > 0) {
             fetch('http://localhost/recette/admin/admin.php?action=recette&nomTable='+inputTab+'&nomChamps='+inputName+'&search='+inputVal)
             .then(function (response) {
                 return response.json();
             }).then(function (liste) {
-                console.log(liste);
                 autocomplete_results = document.getElementById("hugeList_"+inputName);
-                autocomplete_results.innerHTML = '';
                 for (i = 0; i < liste.length; i++) {
-                    autocomplete_results.innerHTML += '<option>' + liste[i] + '</option>';
+                    autocomplete_results_HTML = document.createElement('option');
+                    autocomplete_results_HTML.innerText = liste[i];
+                    autocomplete_results_HTML.addEventListener('click', function (e) {
+                        e.preventDefault();
+                        document.getElementById(inputName).value = this.label;
+                    });
+                    autocomplete_results.appendChild(autocomplete_results_HTML)
                 }
                 autocomplete_results.style.display = 'block';
-            });            
-            
+            });
         }
     }
 }
