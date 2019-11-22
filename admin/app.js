29b3c89c47bf83b02e5b0006b15219f12cd1614c
Damien GAULTIER <dgaultier@FORMATION.lan>
2019-11-22 10:24:32 +0100

    auto_complete terminer

--- admin/app.js
+++ admin/app.js
@@ -3,15 +3,18 @@ var inputs = document.querySelectorAll('input');
 // events
 for (var input of inputs ){
     input.onkeyup = function(e) {
-        input_name = this.name;
-        input_val = this.value; // updates the variable on each ocurrence
-        let nomChamp = 'nom'+input_name[0].toUpperCase()+input_name.substr(1);
-        if (input_val.length > 0) {
-        fetch('http://localhost/recette/admin/admin.php?action=recette&nomTable='+input_name+'&nomChamps='+nomChamp+'&search='+input_val)
+        inputName = this.name;
+        inputTab = this.getAttribute('data-tab');
+        inputVal = this.value; // updates the variable on each ocurrence
+        //let nomChamp = 'nom'+input_name[0].toUpperCase()+input_name.substr(1);
+        console.log(inputTab, inputName, inputVal);
+        if (inputVal.length > 0) {
+            fetch('http://localhost/recette/admin/admin.php?action=recette&nomTable='+inputTab+'&nomChamps='+inputName+'&search='+inputVal)
             .then(function (response) {
                 return response.json();
             }).then(function (liste) {
-                autocomplete_results = document.getElementById("hugeList_"+input_name);
+                console.log(liste);
+                autocomplete_results = document.getElementById("hugeList_"+inputTab);
                 autocomplete_results.innerHTML = '';
                 for (i = 0; i < liste.length; i++) {
                     autocomplete_results.innerHTML += '<option>' + liste[i] + '</option>';
--- admin/auto_complete.php
+++ admin/auto_complete.php
@@ -1,30 +1,19 @@
 <?php
 
-
 if (isset($_GET['nomTable']) && isset($_GET['nomChamps'])) {
     $requete = "SELECT ". $_GET['nomChamps'] ." FROM ".$_GET['nomTable'];
     if (isset($_GET['search'])) {
         $requete .= " WHERE ".$_GET['nomChamps']." LIKE '%".$_GET['search']."%'";
     }
+    $requete.= "LIMIT 5";
     $resultat=mysqli_query($connexion,$requete);
     $array = [];
     while($tab=mysqli_fetch_array($resultat)) {
-        $array[] =  $tab[0];
+        $array[] =  utf8_encode($tab[0]);
     }
     print json_encode($array);
     die;
 }
-
-
-
-$array = '[';
-$requete = "SELECT nomCategorie FROM categorie";
-$resultat=mysqli_query($connexion,$requete);
-while($tab=mysqli_fetch_array($resultat)) {
-    $array .= '"'.$tab[0].'",';
-}
-$array .= ']';
-
 
 $contenu = "form_recette.html";
 ?>
