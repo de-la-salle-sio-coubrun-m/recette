6368db74ea80c5ea4f1b29856f0441b4accd9e95
Kevin POMMERIE <kpommerie@FORMATION.lan>
2019-12-09 14:34:18 +0100

    formulaire de contact envoie mail

--- front/nouscontacter.php
+++ front/nouscontacter.php
@@ -14,7 +14,8 @@
     <section>
         <div id="contact">
             <form action="" method="POST">
-                <input placeholder="Nom utilisateur" type="text" name="nom_contact" value="<?php if(isset($_POST['nom_contact'])){echo $_POST['nom_contact'];} ?>"  />
+                <input placeholder="Prénom" type="text" name="first_name" value="<?php if(isset($_POST['first_name'])){echo $_POST['first_name'];} ?>"  />
+                <input placeholder="Nom" type="text" name="last_name" value="<?php if(isset($_POST['last_name'])){echo $_POST['last_name'];} ?>"  />
                 <input placeholder="Email" type="email" name="email_contact" value="<?php if(isset($_POST['email_contact'])){echo $_POST['email_contact'];} ?>" />
                 <textarea placeholder="Message" name="message_contact" rows="10" cols="50"><?php if(isset($_POST['message_contact'])){echo $_POST['message_contact'];} ?></textarea>
                 
@@ -23,9 +24,25 @@
                 
                 <label><a href="#pol_conf">voir la politique de confidentialité</a></label> -->
                 
-                <?php if(isset($message)){echo $message;} ?>
+                <!--?php if(isset($message)){echo $message;} ?-->
                 
                 <input type="submit" name="submit" value="ENVOYER" />
+
+                <?php
+                if(isset($_POST['submit'])){
+                    $to = "kazumixxx69@gmail.com";
+                    $from = $_POST['email_contact'];
+                    $first_name = $_POST['first_name'];
+                    $last_name = $_POST['last_name'];
+                    $subject = "Form submission";
+                    $message = $first_name . " " . $last_name . "wrote the following :\n\n" . $_POST['message_contact'];
+
+                    $headers = "From:" . $from;
+                    //mail($to,$subject,$message,$headers); // ne fonctionne que sur un vrai serveur, pas en local
+                    echo "Message envoyé. Merci " . $first_name . ", nous vous recontacterons bientôt.";
+                }
+                ?>
+
             </form>
             <!-- <div class="pol_conf">
                 <a href="front.php?page=contact#contact">X</a>
