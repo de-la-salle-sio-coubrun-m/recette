7836fad95b3da81f3c672678313a80a4d7d27816
Damien GAULTIER <dgaultier@FORMATION.lan>
2019-12-11 09:44:33 +0100

    responssive du front

--- front/recette.php
+++ front/recette.php
@@ -9,15 +9,21 @@
 
 <section id="ficheRecette">
     <h2><?php if(isset($recette)){ echo $recette->nomRecette; }?></h2>
-    <article>
-        <img src="<?php if(isset($image)){ echo $image->urlImage ;} ?>" alt="<?php if(isset($recette)){ echo $recette->nomRecette; }?>">
-        <ul>
-            <li>Effets de la recette: <?php if(isset($recette)){ echo $recette->effetsRecette; }?></li>
-            <li>Durée de la préparation: <?php if(isset($recette)){ echo $recette->dureePreparation; }?></li>
-            <li>Durée de la cuisson: <?php if(isset($recette)){ echo $recette->dureeCuisson; }?></li>
-            <li>Description de la recette: <?php if(isset($recette)){ echo $recette->descriptionRecette; }?></li>
-        </ul>
-        <div>
+    <article class="container">
+        <div class="row">
+            <figure class="col-md-8">
+                <img src="<?php if(isset($image)){ echo $image->urlImage ;} ?>" alt="<?php if(isset($recette)){ echo $recette->nomRecette; }?>">
+            </figure>
+            <div class="col">
+                <ul>
+                    <li>Effets de la recette: <?php if(isset($recette)){ echo $recette->effetsRecette; }?></li>
+                    <li>Durée de la préparation: <?php if(isset($recette)){ echo $recette->dureePreparation; }?></li>
+                    <li>Durée de la cuisson: <?php if(isset($recette)){ echo $recette->dureeCuisson; }?></li>
+                    <li>Description de la recette: <?php if(isset($recette)){ echo $recette->descriptionRecette; }?></li>
+                </ul>
+            </div>
+        </div>
+        <div class="col-md-12">
             <h3>La recette :</h3>
             <p><?php if(isset($recette)){ echo $recette->recetteRecette; }?></p>
         </div>
