97137cd2573aacebda96d08a82cbf1e1bd8a37a8
Mickaël COUBRUN <mcoubrun@FORMATION.lan>
2019-12-12 15:51:09 +0100

    unArticle

--- front/article.php
+++ front/article.php
@@ -1,7 +1,7 @@
 <?php 
     include('header.html');
     $base = connect();
-    $article= $base->query("SELECT * FROM article WHERE idArticle='".$_GET['idarticle']."'")->fetch(PDO::FETCH_OBJ);
+    $article= $base->query("SELECT * FROM article WHERE idArticle='".$_GET['idArticle']."'")->fetch(PDO::FETCH_OBJ);
     $image= $base->query("SELECT urlImage FROM image, recette WHERE recette.idImage = image.idImage AND idRecette = '".$article->idRecette."'")->fetch(PDO::FETCH_OBJ);
     $image->urlImage = str_replace("_p", "_g", $image->urlImage);
     $auteur=$base->query("SELECT nomMembre FROM membre WHERE idMembre='".$article->idMembre."'")->fetch(PDO::FETCH_OBJ);
@@ -9,16 +9,23 @@
 ?>
 
 <section id="ficheArticle">
-    <article>
-        <img src="<?php if(isset($image)){ echo $image->urlImage ;} ?>" alt="<?php if(isset($article)){ echo $article->titreArticle; }?>">
-        <ul>
-            <li>Auteur de l'article: <?php if(isset($article)){ echo $auteur->nomMembre; }?></li>
-            <li>Date de publication: <?php if(isset($article)){ echo $article->dateArticle; }?></li>
-            <li>Recette: <?php if(isset($article)){ echo $recette->nomRecette; }?></li>
-        </ul>
-        <div>
-            <h2><?php if(isset($article)){ echo $article->titreArticle; }?></h2>
-            <p><?php if(isset($article)){ echo $article->contenuArticle; }?></p>
+    <h1><?php if(isset($article)){ echo $article->titreArticle; }?></h1>
+    <article class="container">
+        <div class="row">
+        <figure class="col-md-8">
+                <img src="<?php if(isset($image)){ echo $image->urlImage ;} ?>" alt="<?php if(isset($article)){ echo $article->titreArticle; }?>">
+            </figure>
+            <div class="col">
+                <ul>
+                    <li>Auteur de l'article: <?php if(isset($article)){ echo $auteur->nomMembre; }?></li>
+                    <li>Date de publication: <?php if(isset($article)){ echo $article->dateArticle; }?></li>
+                    <li>Recette: <?php if(isset($article)){ echo $recette->nomRecette; }?></li>
+                </ul>
+            </div>
+            <div>
+                <!-- <h2><?php //if(isset($article)){ echo $article->titreArticle; }?></h2> -->
+                <p><?php if(isset($article)){ echo $article->contenuArticle; }?></p>
+            </div>
         </div>
     </article>
 </section>
