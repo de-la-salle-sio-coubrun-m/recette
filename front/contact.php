3dd54a775c6fc78bd2d0d68c2f80228add7d36eb
Damien GAULTIER <dgaultier@FORMATION.lan>
2019-12-12 17:08:49 +0100

    suppression pages "contact" vide

--- front/contact.php
+++
@@ -1,9 +0,0 @@
-<?php include('header.html'); ?>
-
-    <div>16, champ de l'USS Enterprise, Bourg-Palette</div>
-    <div>Tél : 05 55 12 34 56</div>
-    <div>onmangebien@lacantina.fr</div>
-    
-    <?php
-  include('footer.html');
-  ?>
