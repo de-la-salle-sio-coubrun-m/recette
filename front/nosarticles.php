8c99642d70328fcb53789d8c088532bbd5639e40
Mickaël COUBRUN <mcoubrun@FORMATION.lan>
2019-12-12 16:03:04 +0100

    article

--- front/nosarticles.php
+++ front/nosarticles.php
@@ -7,15 +7,17 @@
       $requete= $base->query("SELECT * FROM article ORDER BY dateArticle DESC");
       while($ligne= $requete->fetch(PDO::FETCH_OBJ)){
         $image = $base->query("SELECT urlImage FROM image, recette WHERE recette.idImage = image.idImage AND idRecette = '".$ligne->idRecette."'")->fetch(PDO::FETCH_OBJ);
-        $thumbnail = '<article class="miniRecette">'; 
+        $thumbnail = '<figure class="miniRecette">'; 
         $thumbnail .= '<a href="article.php?idArticle='.$ligne->idArticle.'">';       
         $image->urlImage = str_replace("_p", "_g", $image->urlImage);
         $thumbnail .= '<img src="'.$image->urlImage.'" alt="'.$ligne->titreArticle.'">';
-        $thumbnail .= '<div><h3>'.$ligne->titreArticle.'</h3>';
+        $thumbnail .= '<figcaption>'.$ligne->titreArticle.'</figcaption>';
+        $thumbnail .='</a>';
+        $thumbnail .='</figure>';
         // $thumbnail .= '<ul><li>Date: '.$ligne->dateArticle.'</li>';
         // $thumbnail .= '<li>Notre article: </li>';
         // $thumbnail .= '<li>'.$ligne->contenuArticle.'</li></ul>';
-        $thumbnail .= '</div></article>';
+        // $thumbnail .= '</div></article>';
         echo $thumbnail;
       }
     ?>
